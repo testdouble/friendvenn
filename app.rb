bd0bc4d82a16df0ddf37c98127e3a7e64e1bb09c
Justin Searls <searls@gmail.com>
2013-01-28 21:04:01 -0500

    rip out stuff i don't need

--- app.rb
+++ app.rb
@@ -1,70 +1,36 @@
-%w(omniauth omniauth-twitter dm-core dm-sqlite-adapter dm-migrations sinatra).each { |dependency| require dependency }
+require 'omniauth'
+require 'omniauth-twitter'
+require 'sinatra'
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/database.db")
-
-class User
-  include DataMapper::Resource
-  property :id,         Serial
-  property :uid,        String
-  property :name,       String
-  property :nickname,   String
-  property :created_at, DateTime
-end
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
-
-# You'll need to customize the following line. Replace the CONSUMER_KEY
-#   and CONSUMER_SECRET with the values you got from Twitter
-#   (https://dev.twitter.com/apps/new).
 use OmniAuth::Strategies::Twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
 
 enable :sessions
 
 helpers do
-  def current_user
-    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+  def current_user_id
+    @current_user ||= session[:user_id]
   end
 end
 
 get '/' do
-  if current_user
-    # The following line just tests to see that it's working.
-    #   If you've logged in your first user, '/' should load: "1 ... 1";
-    #   You can then remove the following line, start using view templates, etc.
-    current_user.id.to_s + " ... " + session[:user_id].to_s
+  if current_user_id
+    "Hi #{current_user_id}! <a href='/sign_out'>sign out</a>"
   else
-    '<a href="/sign_up">create an account</a> or <a href="/sign_in">sign in with Twitter</a>'
-    # if you replace the above line with the following line,
-    #   the user gets signed in automatically. Could be useful.
-    #   Could also break user expectations.
-    # redirect '/auth/twitter'
+    '<a href="/sign_in">sign in with Twitter</a>'
   end
 end
 
 get '/auth/:name/callback' do
   auth = request.env["omniauth.auth"]
-  user = User.first_or_create({ :uid => auth["uid"]}, {
-    :uid => auth["uid"],
-    :nickname => auth["info"]["nickname"],
-    :name => auth["info"]["name"],
-    :created_at => Time.now })
-  session[:user_id] = user.id
+  session[:user_id] = auth["uid"]
   redirect '/'
 end
 
-# any of the following routes should work to sign the user in:
-#   /sign_up, /signup, /sign_in, /signin, /log_in, /login
-["/sign_in/?", "/signin/?", "/log_in/?", "/login/?", "/sign_up/?", "/signup/?"].each do |path|
-  get path do
-    redirect '/auth/twitter'
-  end
+get '/sign_in/?' do
+  redirect '/auth/twitter'
 end
 
-# either /log_out, /logout, /sign_out, or /signout will end the session and log the user out
-["/sign_out/?", "/signout/?", "/log_out/?", "/logout/?"].each do |path|
-  get path do
-    session[:user_id] = nil
-    redirect '/'
-  end
+get '/sign_out' do
+  session[:user_id] = nil
+  redirect '/'
 end
